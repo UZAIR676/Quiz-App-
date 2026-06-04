4c831cc7b4b93eec2b657648ca6d94309e411179
uzair rana <you@example.com>
2026-06-04 00:21:05 -0400

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-   proxy: {
-  '/api': 'https://quiz-app-production-be4e.up.railway.app'
-}
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
   }
 })
